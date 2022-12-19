747bd27361cdb9a4dbe07fb1585f73dffed04426
kate.hunter@usu.edu <kate.hunter@usu.edu>
2022-12-19 11:28:43 -0700

    12.19.22 commit

--- scripts/snake/khprac/bash_snake.sh
+++ scripts/snake/khprac/bash_snake.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --partition=kapheim-shared-np
 #SBATCH --account=kapheim-np
-#SBATCH --mem=6GB
-#SBATCH --ntasks 19
+#SBATCH --mem=3GB
+#SBATCH --ntasks 5
 
 #bash script for executing snakemake in cluster
 #important note yaml files need to have exactly four spaces to constitute an indent
 
 
 module load snakemake/6.4.1
-	snakemake -s snakefile --cluster-config cluster.yaml --jobs 19 \
-	--cluster "sbatch --ntasks=19 --time 1:00:00 --mem={resources.mem_mb} --cpus-per-task={params.cpu} -M {cluster.cluster} -A {cluster.account} -p {cluster.partition}" \
+	snakemake -s snakefile --cluster-config cluster.yaml --jobs 5 \
+	--cluster "sbatch --ntasks=5 --time 1:00:00 --mem={resources.mem_mb} --cpus-per-task={params.cpu} -M {cluster.cluster} -A {cluster.account} -p {cluster.partition}" \
 	--latency-wait 10
 
 #To run bash script, type in sbatch bash_snake.sh
