39765210a10baa7c101970b8c70340e0d6ff3df1
Kate Hunter <kate.hunter@usu.edu>
2023-02-07 12:32:04 -0700

    snakefile almost done

--- snake/kh_snakes/bash_snake.sh
+++ snake/kh_snakes/bash_snake.sh
@@ -2,15 +2,15 @@
 #SBATCH --partition=kapheim-shared-np
 #SBATCH --account=kapheim-np
 #SBATCH --mem=4GB
-#SBATCH --ntasks 20
+#SBATCH --ntasks 4
 
 #bash script for executing snakemake in cluster
 #important note yaml files need to have exactly four spaces to constitute an indent
 
 
 module load snakemake/6.4.1
-	snakemake -s snakefile --cluster-config cluster.yaml --jobs 20 \
-	--cluster "sbatch --ntasks=20 --time 1:00:00 --mem={resources.mem_mb} --cpus-per-task={params.cpu} -M {cluster.cluster} -A {cluster.account} -p {cluster.partition}" \
+	snakemake -s snakefile --cluster-config cluster.yaml --jobs 4 \
+	--cluster "sbatch --ntasks=4 --time 2:00:00 --mem={resources.mem_mb} --cpus-per-task={params.cpu} -M {cluster.cluster} -A {cluster.account} -p {cluster.partition}" \
 	--latency-wait 10
 
 #To run bash script, type in sbatch bash_snake.sh
